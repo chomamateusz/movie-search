c67cd797242ae24dc1f4a9cba301c0c6ad0315df
Mateusz Choma <chomamateusz@gmail.com>
2020-11-23 09:32:29 +0100

    ESLInt added

---
+++ .eslintrc.js
@@ -0,0 +1,150 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: false
+  },
+  ignorePatterns: ["coverage", "dist", "build"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    sourceType: "module"
+  },
+  extends: ['react-app',],
+  plugins: ["prefer-arrow", "import", "@typescript-eslint"],
+  rules: {
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "never",
+      "exports": "never",
+      "functions": "never"
+    }],
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/ban-types": "error",
+    "@typescript-eslint/class-name-casing": "off",
+    "@typescript-eslint/consistent-type-assertions": "error",
+    "@typescript-eslint/consistent-type-definitions": "error",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      {
+        accessibility: "explicit"
+      }
+    ],
+    "@typescript-eslint/indent": [
+      "warn",
+      2,
+      {
+        ignoredNodes: ['TSTypeParameterInstantiation'],
+        SwitchCase: 1,
+        FunctionDeclaration: {
+          parameters: "first"
+        },
+        FunctionExpression: {
+          parameters: "first"
+        }
+      }
+    ],
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        multiline: {
+          delimiter: "comma",
+          requireLast: true
+        },
+        singleline: {
+          delimiter: "comma",
+          requireLast: false
+        }
+      }
+    ],
+    "@typescript-eslint/member-ordering": "error",
+    "@typescript-eslint/no-empty-function": "error",
+    "@typescript-eslint/no-empty-interface": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-misused-new": "error",
+    "@typescript-eslint/no-namespace": "error",
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-var-requires": "error",
+    "@typescript-eslint/prefer-for-of": "error",
+    "@typescript-eslint/prefer-function-type": "error",
+    "@typescript-eslint/prefer-namespace-keyword": "error",
+    "@typescript-eslint/quotes": [
+      "error",
+      "single",
+      {
+        avoidEscape: true
+      }
+    ],
+    "@typescript-eslint/semi": ["error", "never"],
+    "@typescript-eslint/triple-slash-reference": "error",
+    "@typescript-eslint/type-annotation-spacing": "error",
+    "@typescript-eslint/unified-signatures": "error",
+    "arrow-parens": ["error", "always"],
+    camelcase: "error",
+    "capitalized-comments": "off",
+    "comma-dangle": ["error", "always-multiline"],
+    complexity: "off",
+    "constructor-super": "error",
+    "dot-notation": "error",
+    "eol-last": "error",
+    eqeqeq: ["error", "smart"],
+    "guard-for-in": "error",
+    "id-blacklist": ["error", "any", "Number", "number", "String", "string", "Boolean", "boolean", "Undefined", "undefined"],
+    "id-match": "error",
+    "import/order": "off",
+    "max-classes-per-file": ["error", 1],
+    "max-len": [
+      "error",
+      {
+        "ignoreUrls": true,
+        code: 160
+      }
+    ],
+    "new-parens": "error",
+    "no-bitwise": "error",
+    "no-caller": "error",
+    "no-cond-assign": "error",
+    "no-console": "off",
+    "no-debugger": "error",
+    "no-empty": "error",
+    "no-eval": "error",
+    "no-fallthrough": "off",
+    "no-invalid-this": "off",
+    "no-multiple-empty-lines": "error",
+    "no-new-wrappers": "error",
+    "no-shadow": [
+      "error",
+      {
+        hoist: "all"
+      }
+    ],
+    "no-throw-literal": "error",
+    "no-trailing-spaces": "error",
+    "no-undef-init": "error",
+    "no-unsafe-finally": "error",
+    "no-unused-expressions": "error",
+    "no-unused-labels": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }],
+    "no-var": "error",
+    "object-shorthand": "error",
+    "one-var": ["error", "never"],
+    "prefer-const": "error",
+    "quote-props": ["error", "consistent-as-needed"],
+    radix: "error",
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "never",
+        asyncArrow: "always",
+        named: "never"
+      }
+    ],
+    "spaced-comment": "error",
+    "use-isnan": "error",
+    "valid-typeof": "off",
+  }
+};
